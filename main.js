f85681026da25c8f5aba06ddff90129e83a629f9
matias <triangulacionesrb@gmail.com>
2023-02-28 21:00:54 -0300

    Modifque varias cosas

--- main.js
+++ main.js
@@ -8,6 +8,17 @@ if(screen.width <600){
   
 };
 
+
+if(screen.width <1000){
+    
+   const div = document.querySelector(".hola");
+  
+   div.remove();
+   
+  
+};
+
+
 if(screen.height <500){
     
    const div = document.querySelector(".hola");
